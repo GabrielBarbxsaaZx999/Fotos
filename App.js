6cada615e6906ded6a18163184202835435bd72d
GABRIEL SO$A <125594732+GabrielBarbxsaaZx999@users.noreply.github.com>
2024-02-29 13:58:43 -0400

    Update App.js

--- App.js
+++ App.js
@@ -9,33 +9,39 @@ function HomeScreen({ navigation }) {
       source={{ uri: 'https://preview.redd.it/kanye-west-graduation-3840x2160-v0-kjektcn4myv91.png?auto=webp&s=09ab551caf64aa606dc8720953ad6abaca36c079' }}
       style={{ flex: 1, alignItems: 'center', justifyContent: 'center', resizeMode: 'cover' }}
     >
-      <View style={{ backgroundColor:   '#fff', padding: 20, borderRadius: 10 }}>
-        <Text style={{ fontSize: 40, fontFamily: 'Arial', fontWeight: 'bold', color: '#333', color:'black' }}>
+      <View style={{ backgroundColor: '#fff', padding: 20, borderRadius: 10 }}>
+        <Text style={{ fontSize: 40, fontFamily: 'Arial', fontWeight: 'bold', color: '#333', color: 'black' }}>
           5 Lugares favoritos do SENAI-Sumaré
         </Text>
         <Button
           title="Jardim da parte de trás"
           onPress={() => navigation.navigate('Jardim')}
+          color="#FF5733"  // Cor personalizada
         />
         <Button
           title="Biblioteca"
           onPress={() => navigation.navigate('Biblioteca')}
+          color="#3498db"  // Cor personalizada
         />
 
         <Button
           title="Biblioteca 2.0"
           onPress={() => navigation.navigate('Biblioteca 2.0')}
+          color="#8e44ad"  // Cor personalizada
         />
 
         <Button
           title="Mesas do al mosso"
           onPress={() => navigation.navigate('Almosso')}
+          color="#27ae60"  // Cor personalizada
         />
 
         <Button
           title="Saída florida"
           onPress={() => navigation.navigate('Saida')}
+          color="#e74c3c"  // Cor personalizada
         />
+
 
       </View>
     </ImageBackground>
@@ -46,13 +52,13 @@ function JardimScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 
-<Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                   Escolhemos essa parte porque é legal e bonita!
-                  </Text>
-                  
+      <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
+        Escolhemos essa parte porque é legal e bonita!
+      </Text>
+
       <Image
-       source={require('./jardim.jpeg')}
-        style={{ width: 800, height:600  }}
+        source={require('./jardim.jpeg')}
+        style={{ width: 800, height: 600 }}
       />
 
 
@@ -64,11 +70,11 @@ function BiblioScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                  A biblioteca tem muitas atividades legais e a interação social lá é intensa
-                  </Text>
+        A biblioteca tem muitas atividades legais e a interação social lá é intensa
+      </Text>
       <Image
-          source={require('./biblio.jpeg')}
-          style={{ width: 800, height:600  }}
+        source={require('./biblio.jpeg')}
+        style={{ width: 800, height: 600 }}
       />
     </View>
   );
@@ -76,14 +82,14 @@ function BiblioScreen() {
 
 function Bib2Screen() {
   return (
-    
+
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                   A biblioteca merece duas fotos porque é muito bacana
-                  </Text>
+        A biblioteca merece duas fotos porque é muito bacana
+      </Text>
       <Image
         source={require('./bib2.jpeg')}
-        style={{ width: 800, height:600  }}
+        style={{ width: 800, height: 600 }}
       />
     </View>
   );
@@ -93,11 +99,11 @@ function AlmossoScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                  Almoçar nessa parte é supimpa porque podemos refletir comer e desfrutar da nossa bela vida
-                  </Text>
+        Almoçar nessa parte é supimpa porque podemos refletir comer e desfrutar da nossa bela vida
+      </Text>
       <Image
         source={require('./almosso.jpeg')}
-        style={{ width: 800, height:600  }}
+        style={{ width: 800, height: 600 }}
       />
     </View>
   );
@@ -107,11 +113,11 @@ function SaidaScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text style={{ fontSize: 25, fontFamily: 'Arial', fontWeight: 'bold', color: '#333' }}>
-                 Essa parte da saida é bonita e nos traz o pensamentos de uma das melhores partes do SENAI
-                  </Text>
+        Essa parte da saida é bonita e nos traz o pensamentos de uma das melhores partes do SENAI
+      </Text>
       <Image
         source={require('./saida.jpeg')}
-        style={{ width: 800, height:600  }}
+        style={{ width: 800, height: 600 }}
       />
     </View>
   );
